245b66104c38f595c200cc2e2f9936fdfdacee46
nazmossakib <nazmos@sakib.com>
2017-04-06 11:40:47 +0600

    updated

---
+++ footer.php
@@ -0,0 +1,38 @@
+
+    <div style="padding-top: 20px">
+        <div style="background-color: whitesmoke;width: 100%;height: 100px;border: 2px #a7ffb4 solid">
+            <div style="background-color: '';width: 50%;height: 100%;float:left">
+                <div style="width: 10%;height: 100%;background-color:'';float:left;padding-left: 15%">
+                    <img src="image/hatbazar.jpg" alt="hat-bazer-logo" style="width: 100%;height: 50%;padding-top: 30%">
+                </div>
+                <div style="float:left">
+
+                    <p><span style="font-weight: bold;">Hat Bazar in your pocket!</span><br>Get 250 TK welcome voucher & faster access to deals!</p>
+                </div>
+            </div>
+
+            <div style="background-color: #4CAF50;width: 50%;height: 100%;float:left">
+
+            </div>
+        </div>
+
+        <div style="background-color: #555555;width: 100%;height: 300px;">
+            <div style="background-color: aqua;width: 23%;height: 100%;float:left">
+
+            </div>
+
+            <div style="background-color: blue;width: 23%;height: 100%;float:left">
+
+            </div>
+
+            <div style="background-color: blueviolet;width: 24%;height: 100%;float:left">
+
+            </div>
+
+            <div style="background-color: chocolate;width: 30%;height: 100%;float:left">
+
+            </div>
+        </div>
+
+    </div>
+
